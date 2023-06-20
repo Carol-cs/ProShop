45eff85609f7002a219555d190ff6ede30440895
Carol Ma <carolm03@students.cs.ubc.ca>
2023-06-20 02:00:29 -0700

    Cart Screen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import { FaShoppingCart, FaUser } from 'react-icons/fa'
-import logo from '../assets/logo.png'
-import {Link} from 'react-router-dom'
-
-
+import React from "react";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
@@ -14,18 +15,29 @@ const Header = () => {
           <Navbar.Brand as={Link} to="/">
             <img src={logo} alt="ProShop" />
             ProShop
-            </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav" >
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/cart"><FaShoppingCart />Cart</Nav.Link>
-              <Nav.Link as={Link} to="login"><FaUser />Sign In</Nav.Link>
+              <Nav.Link as={Link} to="/cart">
+                <FaShoppingCart />
+                Cart
+                {cartItems.length > 0 && (
+                  <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                  </Badge>
+                )}
+              </Nav.Link>
+              <Nav.Link as={Link} to="login">
+                <FaUser />
+                Sign In
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
