cd92055808be735e0778109b2c4b225c07d0f1a9
Carol Ma <carolm03@students.cs.ubc.ca>
2023-06-20 04:35:57 -0700

    Cart Screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,39 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider
-} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import store from './store'
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 // import 'bootstrap/dist/css/bootstrap.min.css'
-import "./assets/styles/bootstrap.custom.css"
-import './assets/styles/index.css'
-import App from './App';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import reportWebVitals from './reportWebVitals';
-
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import reportWebVitals from "./reportWebVitals";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App/>}>
-      <Route index element={<HomeScreen/>}/>
-      <Route path="/product/:id" element={<ProductScreen/>}/>
-      
-
-
+    <Route path="/" element={<App />}>
+      <Route index element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+      <Route path="/cart" element={<CartScreen />} />
     </Route>
   )
-)
+);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <RouterProvider router={router}/> 
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
