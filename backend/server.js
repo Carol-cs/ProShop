91de6be168448f43a6281234675928d801bd066d
Carol Ma <carolma@Carols-MacBook-Pro.local>
2023-12-27 17:25:14 -0800

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,9 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware that allows us to access req.cookie
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running...");
+// });
 
 // when we hit /api/products..., we will goto productRoutes file
 app.use("/api/products", productRoutes);
@@ -40,6 +40,20 @@ app.get("/api/config/paypal", (req, res) =>
 
 const __dirname = path.resolve(); // set __dirname to current directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  // set static react build folder
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
